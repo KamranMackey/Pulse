6381372f11ab71f4efb0e32615db690dc2802bcf
Kamran Mackey <kamranm1200@gmail.com>
2019-09-15 01:36:34 -0600

    Implemented a popup menu.
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- app/src/main/java/com/kamranmackey/MainFragment.kt
+++ app/src/main/java/com/kamranmackey/MainFragment.kt
@@ -1,24 +1,21 @@
 package com.kamranmackey
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.kamranmackey.pulse.R
 
-/**
- * A simple [Fragment] subclass.
- */
 class MainFragment : Fragment() {
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.fragment_main, container, false);
-    }
 
     companion object {
         fun newInstance() = MainFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
 }
--- app/src/main/java/com/kamranmackey/pulse/MainActivity.kt
+++ app/src/main/java/com/kamranmackey/pulse/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.kamranmackey.pulse
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.kamranmackey.MainFragment
 import com.kamranmackey.pulse.databinding.ActivityMainBinding
+import com.kamranmackey.pulse.utils.extensions.Context.popup
 import com.kamranmackey.pulse.utils.extensions.Context.toast
 
 class MainActivity : AppCompatActivity() {
@@ -12,22 +14,23 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
-
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
         binding.toolbarTitle.setOnLongClickListener { toast("Hello Long Click") }
         binding.searchAction.setOnClickListener { toast("Hello Search") }
-        binding.settingsAction.setOnClickListener { toast("Hello Settings") }
+        binding.overflowMenu.setOnClickListener { popup(binding.overflowMenu, R.menu.menu_main)}
 
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
+    }
+
+    private fun showOverflow(view: View) {
+
     }
 }
