3476e403b2da6f98d4f9ddf9900f7ae5f99722af
Kamran Mackey <kamranm1200@gmail.com>
2019-10-02 18:15:57 -0600

    Display album name besides the artist.
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- app/src/main/java/com/kamranmackey/pulse/backend/adapters/SongAdapter.kt
+++ app/src/main/java/com/kamranmackey/pulse/backend/adapters/SongAdapter.kt
@@ -1,5 +1,6 @@
 package com.kamranmackey.pulse.backend.adapters
 
+import android.text.SpannableStringBuilder
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,13 +10,13 @@ import com.kamranmackey.pulse.R
 import com.kamranmackey.pulse.backend.models.Song
 
 
-class SongAdapter(private val moviesList: List<Song>) :
+class SongAdapter(private val songList: List<Song>) :
     RecyclerView.Adapter<SongAdapter.ViewHolder>() {
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         var title: TextView = view.findViewById(R.id.title)
-        var artist: TextView = view.findViewById(R.id.year)
-        var year: TextView = view.findViewById(R.id.genre)
+        var artist: TextView = view.findViewById(R.id.artist)
+        var year: TextView = view.findViewById(R.id.year)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -26,11 +27,20 @@ class SongAdapter(private val moviesList: List<Song>) :
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val movie = moviesList[position]
-        holder.title.text = movie.title
-        holder.year.text = movie.artist
-        holder.artist.text = movie.year.toString()
+        val song = songList[position]
+        val string = SpannableStringBuilder()
+
+        val title = song.title
+        val artist = song.artist
+        val album = song.album
+        val year = song.year.toString()
+
+        string.append(artist).append(" • ").append(album)
+
+        holder.title.text = title
+        holder.artist.text = string
+        holder.year.text = year
     }
 
-    override fun getItemCount(): Int = moviesList.size
+    override fun getItemCount(): Int = songList.size
 }
