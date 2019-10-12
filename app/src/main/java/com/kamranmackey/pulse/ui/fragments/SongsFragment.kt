4dcab8303ff89e4dd554c895a5fde956a733b3bc
Kamran Mackey <kamranm1200@gmail.com>
2019-10-12 00:38:05 -0600

    Cast fragmentManager to FragmentManager.
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- app/src/main/java/com/kamranmackey/pulse/ui/fragments/SongsFragment.kt
+++ app/src/main/java/com/kamranmackey/pulse/ui/fragments/SongsFragment.kt
@@ -51,7 +51,7 @@ class SongsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        mManager = fragmentManager!!
+        mManager = fragmentManager as FragmentManager
 
         recyclerView = view.findViewById(R.id.recyclerView)
         mAdapter = SongAdapter(songList, mManager)
